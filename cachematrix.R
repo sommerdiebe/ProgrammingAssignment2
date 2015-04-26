9d4817f8632b10dfb61b0013045c00b0c6256544
sommerdiebe <stefan.wagner@kcl.ac.uk>
2015-04-26 16:52:37 +0200

    updated files

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,8 @@
 
 ## Write a short comment describing this function
 
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
 
 }
@@ -13,37 +15,6 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
-## Introduction
-
-#This second programming assignment will require you to write an R function is able to cache potentially time-consuming computations. For example, taking the mean of a numeric vector is typically a fast operation. However, for a very long vector, it may take too long to compute the mean, especially if it has to be computed repeatedly (e.g. in a loop). If the contents of a vector are not changing, it may make sense to cache the value of the mean so that when we need it again, it can be looked up in the cache rather than recomputed. In this Programming Assignment will take advantage of the scoping rules of the R language and how they can be manipulated to preserve state inside of an R object.
-
-#Example: Caching the Mean of a Vector
-
-#In this example we introduce the <<- operator which can be used to assign a value to an object in an environment that is different from the current environment. Below are two functions that are used to create a special object that stores a numeric vector and cache's its mean.
-
-#The first function, makeVector creates a special "vector", which is really a list containing a function to
-#set the value of the vector
-#get the value of the vector
-#set the value of the mean
-#get the value of the mean
-
-#Functions that cache the inverse of a matrix
-##
-## Usage example:
-##
-## > source('cachematrix.R')
-## > m <- makeCacheMatrix(matrix(c(2, 0, 0, 2), c(2, 2)))
-## > cacheSolve(m)
-## [,1] [,2]
-## [1,]  0.5  0.0
-## [2,]  0.0  0.5
-
-## Create a special "matrix", which is a list containing
-## a function to
-##   - set the value of the matrix
-##   - get the value of the matrix
-##   - set the value of the inverse matrix
-##   - get the value of the inverse matrix
 
 
 #############################################################################################
